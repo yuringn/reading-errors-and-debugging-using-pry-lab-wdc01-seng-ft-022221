feaec080e55e47650645a7bf14bc8c65e88eaeb2
Yuri Ng <yuringuyen312@gmail.com>
2021-02-07 05:21:28 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,18 +1,12 @@
 # don't forget to add: require 'pry'
 
 def generate_star_date
-a =  (rand(100000) + 400000) / 10.0
-puts a
+  (rand(100000) + 400000) / 10.0
 end
-generate_star_date
-
 def state_log(star_date)
   "Captain's Log, star date #{star_date}."
 end
-state_log("Thursday")
-
 def engage
   puts state_log(date)
   date = generate_star_date
 end
-engage
