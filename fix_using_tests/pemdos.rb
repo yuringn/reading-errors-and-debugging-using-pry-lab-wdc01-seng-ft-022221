303a4e70f8fa5048982e5513d67fc94e81f9e52b
Yuri Ng <yuringuyen312@gmail.com>
2021-02-07 05:19:34 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,9 +1,11 @@
 # don't forget to add: require 'pry'
-
+#require"pry"
 def snake_it_up(string)
   if string[0] == "s"
-    10 * "s" + string
+     "s" * 10 + string
+  #  binding.pry
   else
     string
   end
 end
+snake_it_up("suprise")
--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,15 +1,18 @@
 # don't forget to add: require 'pry'
 
 def generate_star_date
-  (rand(100000) + 400000) / 10.0
+a =  (rand(100000) + 400000) / 10.0
+puts a
 end
-
+generate_star_date
 
 def state_log(star_date)
   "Captain's Log, star date #{star_date}."
 end
+state_log("Thursday")
 
 def engage
   puts state_log(date)
   date = generate_star_date
 end
+engage
