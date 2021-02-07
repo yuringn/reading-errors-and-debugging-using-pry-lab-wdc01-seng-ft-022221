2d9be483c11b2f280d5bb7f25d30fdc5af014441
Yuri Ng <yuringuyen312@gmail.com>
2021-02-07 05:22:57 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -4,7 +4,7 @@ def generate_star_date
   (rand(100000) + 400000) / 10.0
 end
 def state_log(star_date)
-  "Captain's Log, star date #{star_date}."
+  puts "Captain's Log, star date #{star_date}."
 end
 def engage
   puts state_log(star_date)
